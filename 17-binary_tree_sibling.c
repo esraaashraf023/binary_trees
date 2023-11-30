39414396b74f8723a5429b724ca7adaa1e4f8a3d
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-11-30 21:41:05 +0200

    task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -6,14 +6,13 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *parent;
-
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
-
-	if (parent->left == node)
-		return (parent->right);
+	if (node->parent->left == node)
+		return node->parent->right;
+	else if (node->parent->right == node)
+		return node->parent->left;
 	else
-		return (parent->left);
+		return (NULL);
 }
