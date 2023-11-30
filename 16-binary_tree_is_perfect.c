27d07da82269ea013c38650918a3892541edf2bc
esraaashraf023 <esraaashraf780567@gmail.com>
2023-11-30 04:51:31 -0800

    add file 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -14,7 +14,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 		size += binary_tree_size(tree->left);
 	if (tree->right != NULL)
 		size += binary_tree_size(tree->right);
-	return size;
+	return (size);
 }
 /**
  * binary_tree_height - Measures the height of a binary tree
@@ -26,22 +26,22 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height, right_height;
 
 	if (tree == NULL)
-		return 0;
+		return (0);
 
 	left_height = binary_tree_height(tree->left);
 
 	right_height = binary_tree_height(tree->right);
 
 	if (left_height > right_height)
-		return left_height + 1;
+		return (left_height + 1);
 	else
-        return right_height + 1;
+		return (right_height + 1);
 }
-/*
+/**
  * binary_tree_is_perfect - checks if a binary tree is perfect.
  * @tree:is a pointer to the root node of the tree to check
  * Return:If tree is NULL, return 0.
-*/
+ */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	size_t height, size;
@@ -52,7 +52,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	height = binary_tree_height(tree);
 	size = binary_tree_size(tree);
 	if (size == ((size_t)1 << height) - 1)
-		return 1;
+		return (1);
 	else
-		return 0;
+		return (0);
 }
